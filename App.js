982914a90051ffed4db565fa820927b0d6faa704
unknown <willjones1996@live.co.uk>
2018-06-03 20:27:36 +0100

    more stuff

--- App.js
+++ App.js
@@ -2,14 +2,6 @@ import React, { Component } from 'react';
 import { AppRegistry, Text, TextInput, View, Alert, Button, Image, ReactDOM, SectionList, FlatList } from 'react-native';
 import { List, ListItem, Avatar } from "react-native-elements";
 
-/* To Do:
-*Make it look pretty
-*Compare with other users
-*Sort out why it doesnt find james dawson!
-*Use the data to create graphs?
-*Try karma for testing
-*Tidy up code
-*/
 
 class userObj {
   username;
@@ -31,9 +23,8 @@ class userObj {
 
 export default class GetUsername extends Component {
   async SearchForUser(userName) {
-    if (userName != null || userName != undefined || userName != "") {
 
-      console.log("username: " + userName);
+    if (userName != null || userName != undefined || userName != "") {
       let obj = {
         method: "GET",
         headers: {
@@ -45,23 +36,25 @@ export default class GetUsername extends Component {
         let response = await fetch("https://api.rocketleaguestats.com/v1/search/players?display_name=" + userName, obj)
         let resJSON = await response.json();
         let length = resJSON.totalResults;
-        console.log("length: " + length);
+        // console.log("length: " + length);
 
 
         if (length === 1) {
-          let avatar = resJSON.data[0].avatar;
-          let platform = resJSON.data[0].platform.name;
-          let wins = resJSON.data[0].stats.wins;
-          let goals = resJSON.data[0].stats.goals;
-          let mvps = resJSON.data[0].stats.mvps;
-          let saves = resJSON.data[0].stats.saves;
-          let shots = resJSON.data[0].stats.shots;
-          let assists = resJSON.data[0].stats.assists;
-          let profileUrl = resJSON.data[0].profileUrl;
-          let signatureUrl = resJSON.data[0].signatureUrl; //this has everything on in but in a picture, use the above for graphs?
+          let dataObj = {
+            "Avatar": resJSON.data[0].avatar,
+            "Platform": resJSON.data[0].platform.name,
+            "Wins": resJSON.data[0].stats.wins,
+            "Goals": resJSON.data[0].stats.goals,
+            "Mvps": resJSON.data[0].stats.mvps,
+            "Saves": resJSON.data[0].stats.saves,
+            "Shots": resJSON.data[0].stats.shots,
+            "Assists": resJSON.data[0].stats.assists,
+            "ProfileUrl": resJSON.data[0].profileUrl,
+            "SignatureUrl": resJSON.data[0].signatureUrl
+          }
 
           //this re renders the component!
-          this.setState({ display: true, length: length++, username: userName, avatar: avatar, platform: platform, wins: wins, goals: goals, mvps: mvps, saves: saves, shots: shots, assists: assists, signatureUrl: signatureUrl });
+          this.setState({ display: true, length: length++, username: userName, singlePlayerData: dataObj });
 
         }
         else if (length > 1) {
@@ -103,7 +96,32 @@ export default class GetUsername extends Component {
           this.setState({ display: "Multiple", multipleData: allUsers })
         }
         else {
-          this.setState({ display: "Not found", userName: "User: " + userName + " not found." });
+          //no users found, search using steams API
+          let obj = {
+            "Method": "GET",
+            "Headers": {
+              "Accept": "Application/json"
+            }
+          }
+          let userCall = await fetch("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=DE17C275FB1AC961556785D2BE883E93&vanityurl=" + userName);
+          let userJSON = await userCall.json();
+          let userId = "";
+          try {
+            userId = userJSON.response.steamid;
+          }
+          catch (e) {
+            userId = undefined;
+          }
+
+          console.log("user id!");
+          console.log(userId);
+
+          if (userId === undefined) {
+            this.setState({ display: "Not found", userName: "User: " + userName + " not found." });
+          }
+          else {
+            this.getUserFromID(userId);
+          }
         }
       }
       catch (err) {
@@ -112,10 +130,64 @@ export default class GetUsername extends Component {
       }
     }
   }
+
+
+  async getUserFromID(userId) {
+    let obj = {
+      method: "GET",
+      headers: {
+        Accept: "Application/json",
+        Authorization: "JB2J33D8O694771DF5OOWMSMLYM7WXM2"
+      }
+    }
+
+    let userData = await fetch("https://api.rocketleaguestats.com/v1/player?unique_id=" + userId + "&platform_id=1", obj);
+    let userJSON = await userData.json();
+    let displayName;
+    let avatar;
+    let platform;
+    let profile;
+    let signatureUrl;
+
+    displayName = userJSON.displayName;
+
+    try {
+      avatar = userJSON.avatar
+
+    }
+    catch (e) {
+      avatar = "";
+    }
+    try {
+      profile = userJSON.profileUrl;
+
+    }
+    catch (e) { }
+
+
+    let dataObj = {
+      "Avatar": avatar,
+      "Platform": userJSON.platform.name,
+      "Wins": userJSON.stats.wins,
+      "Goals": userJSON.stats.goals,
+      "Mvps": userJSON.stats.mvps,
+      "Saves": userJSON.stats.saves,
+      "Shots": userJSON.stats.shots,
+      "Assists": userJSON.stats.assists,
+      "ProfileUrl": profile,
+      "SignatureUrl": userJSON.signatureUrl
+    }
+
+    //this re renders the component!
+    this.setState({ display: true, username: displayName, singlePlayerData: dataObj });
+
+  }
+
   constructor(props) {
     super(props);
-    this.state = { text: '', display: false, length: '', username: '', avatar: '', platform: '', wins: '', goals: '', mvps: '', saves: '', shots: '', assists: '', signatureUrl: '', multipleData: [] };
+    this.state = { text: '', display: false, length: '', username: '', singlePlayerData: {}, multipleData: [] };
     this.SearchForUser = this.SearchForUser.bind(this);
+    this.getUserFromID = this.getUserFromID.bind(this);
   }
 
   render() {
@@ -123,14 +195,16 @@ export default class GetUsername extends Component {
       return (
         <View style={{ alignItems: 'center', padding: 10, }}>
           <Text>Welcome to the Rocket league Stats app </Text>
-          <TextInput style={{ height: 40, width: 200 }} placeholder="Enter username here!" onChangeText={(text) => this.setState({ text })} />
+          <Text>Enter username or steam id</Text>
+          <TextInput style={{ height: 40, width: 200 }} placeholder="Enter username or id here!" onChangeText={(text) => this.setState({ text })} />
           <Button title="Get Stats" color="#ADD8E6" onPress={() => this.SearchForUser(this.state.text)} />
         </View>
       );
     }
     if (this.state.display === true) {
-      //use the react native elements to make this bit looks better!
-      let initials = this.state.username; //get initials from this string
+      let initials = this.state.username.slice(0, 2).toString();
+      console.log("initials!");
+      console.log(initials);
 
       if (this.state.avatar == "") {
         return (
@@ -138,16 +212,16 @@ export default class GetUsername extends Component {
             <Avatar
               large
               rounded
-              title="WJ"
+              title={initials}
               activeOpacity={0.7}
             />
-            <Text>Wins: {this.state.wins} </Text>
-            <Text> Goals: {this.state.goals} </Text>
-            <Text>MVP's: {this.state.mvps}</Text>
-            <Text>Saves: {this.state.saves}</Text>
-            <Text>Shots: {this.state.shots}</Text>
-            <Text>Assists: {this.state.assists}</Text>
-            <Image source={{ uri: this.state.signatureUrl }} style={{ width: 400, height: 100 }} />
+            <Text>Wins: {this.state.singlePlayerData.Wins} </Text>
+            <Text> Goals: {this.state.singlePlayerData.Goals} </Text>
+            <Text>MVP's: {this.state.singlePlayerData.Mvps}</Text>
+            <Text>Saves: {this.state.singlePlayerData.Saves}</Text>
+            <Text>Shots: {this.state.singlePlayerData.Shots}</Text>
+            <Text>Assists: {this.state.singlePlayerData.Assists}</Text>
+            <Image source={{ uri: this.state.singlePlayerData.signatureUrl }} style={{ width: 400, height: 100 }} />
             <Button title="Search Again" color="#00B200" onPress={() => this.setState({ display: false })} />
           </View >
         );
@@ -158,16 +232,16 @@ export default class GetUsername extends Component {
             <Avatar
               large
               rounded
-              source={{ uri: this.state.avatar }}
+              source={{ uri: this.state.singlePlayerData.Avatar }}
               activeOpacity={0.7}
             />
-            <Text>Wins: {this.state.wins} </Text>
-            <Text> Goals: {this.state.goals} </Text>
-            <Text>MVP's: {this.state.mvps}</Text>
-            <Text>Saves: {this.state.saves}</Text>
-            <Text>Shots: {this.state.shots}</Text>
-            <Text>Assists: {this.state.assists}</Text>
-            <Image source={{ uri: this.state.signatureUrl }} style={{ width: 400, height: 100 }} />
+            <Text>Wins: {this.state.singlePlayerData.Wins} </Text>
+            <Text>Goals: {this.state.singlePlayerData.Goals} </Text>
+            <Text>MVP's: {this.state.singlePlayerData.Mvps}</Text>
+            <Text>Saves: {this.state.singlePlayerData.Saves}</Text>
+            <Text>Shots: {this.state.singlePlayerData.Shots}</Text>
+            <Text>Assists: {this.state.singlePlayerData.Assists}</Text>
+            <Image source={{ uri: this.state.singlePlayerData.signatureUrl }} style={{ width: 400, height: 100 }} />
             <Button title="Search Again" color="#00B200" onPress={() => this.setState({ display: false })} />
           </View >
         );
@@ -176,10 +250,13 @@ export default class GetUsername extends Component {
     }
 
     if (this.state.display === "Not found") {
+
       return (
         <View style={{ alignItems: 'center', padding: 10, }}>
           <Text>User '{this.state.text}' not found. </Text>
-          <Button title="Search Again" color="#00B200" onPress={() => this.setState({ display: false })} />
+          <Text>Try entering your steam id instead:</Text>
+          <TextInput style={{ height: 40, width: 200 }} placeholder="Enter steam id" onChangeText={(text) => this.setState({ text })} />
+          <Button title="Search using Steam ID" color="#FF0000" onPress={() => this.getUserFromID(this.state.text)} />
         </View >
       );
     }
