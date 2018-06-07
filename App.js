8d438f96895b1dd268fcc14ebe064a74f0ecf5a7
unknown <willjones1996@live.co.uk>
2018-06-07 20:35:01 +0100

    crap stat leadebroasrd display solution

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text, TextInput, View, Alert, Button, Image, ReactDOM, SectionList, FlatList } from 'react-native';
-import { List, ListItem, Avatar, Header } from "react-native-elements";
+import { List, ListItem, Avatar, Header, ButtonGroup, CheckBox } from "react-native-elements";
 
 
 class userObj {
@@ -23,6 +23,14 @@ class userObj {
 }
 
 export default class GetUsername extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '', display: false, length: '', username: '', singlePlayerData: {}, multipleData: [], menu: '', selectedIndex: 0, stat: '', checked: false, showStats: false };
+    this.SearchForUser = this.SearchForUser.bind(this);
+    this.getUserFromID = this.getUserFromID.bind(this);
+    this.updateIndex = this.updateIndex.bind(this)
+  }
+
   async SearchForUser(userName) {
 
     if (userName != null || userName != undefined || userName != "") {
@@ -183,7 +191,34 @@ export default class GetUsername extends Component {
 
   }
 
-  async getRankedUsers() {
+  async getTopStats(index) {
+    let statToFind;
+    const buttons = ["Mvps", "Goals", "Wins", "Shots", "Saves", "Assists"];
+    switch (index) {
+      case 0:
+        statToFind = "mvps"
+        break;
+      case 1:
+        statToFind = "goals"
+        break;
+      case 2:
+        statToFind = "wins"
+        break;
+      case 3:
+        statToFind = "shots"
+        break;
+      case 4:
+        statToFind = "saves"
+        break;
+      case 5:
+        statToFind = "assists"
+        break;
+    }
+    this.setState({ stat: statToFind });
+
+    //console.log("selected index: " + index);
+    //console.log(statToFind);
+
     let obj = {
       method: "GET",
       headers: {
@@ -191,71 +226,91 @@ export default class GetUsername extends Component {
         Authorization: "JB2J33D8O694771DF5OOWMSMLYM7WXM2"
       }
     }
-    //filter by top!
-    //if that doesnt work then just do seasons?
-    let mvp = await fetch("https://api.rocketleaguestats.com/v1/leaderboard/stat?type=mvps", obj);
+
+    let mvp = await fetch("https://api.rocketleaguestats.com/v1/leaderboard/stat?type=" + statToFind, obj);
     let mvpJSON = await mvp.json();
-    console.log("test");
-    console.log(mvpJSON.length);
     let allUsers = [];
     let avatar;
     let profileUrl;
     let platform;
 
-    //looks like its already supported, and $top doesnt want to work!
-    /*
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 100; i++) {
 
       try {
-        avatar = mvpJSON.data[i].avatar
+        avatar = mvpJSON[i].avatar
       }
       catch (err) {
         avatar = "";
       }
       try {
-        profileUrl = mvpJSON.data[i].profileUrl;
+        profileUrl = mvpJSON[i].profileUrl;
       }
       catch (err) {
         profileUrl = "";
       }
       try {
-        platform = mvpJSON.data[i].platform.name;
+        platform = mvpJSON[i].platform.name;
       }
       catch (e) {
         platform = "";
       }
-      let id = mvpJSON.data[i].uniqueId;
-      let wins = mvpJSON.data[i].stats.wins;
-      let goals = mvpJSON.data[i].stats.goals;
-      let mvps = mvpJSON.data[i].stats.mvps;
-      let saves = mvpJSON.data[i].stats.saves;
-      let shots = mvpJSON.data[i].stats.shots;
-      let assists = mvpJSON.data[i].stats.assists;
-      let signatureUrl = mvpJSON.data[i].signatureUrl;
+      let id = mvpJSON[i].uniqueId;
+      let wins = mvpJSON[i].stats.wins;
+      let goals = mvpJSON[i].stats.goals;
+      let mvps = mvpJSON[i].stats.mvps;
+      let saves = mvpJSON[i].stats.saves;
+      let shots = mvpJSON[i].stats.shots;
+      let assists = mvpJSON[i].stats.assists;
+      let signatureUrl = mvpJSON[i].signatureUrl;
+      let userName = mvpJSON[i].displayName;
       let user = new userObj(userName, avatar, platform, profileUrl, id, wins, goals, mvps, saves, assists, signatureUrl, shots);
       allUsers.push(user);
     }
-    this.setState({ display: "Multiple", multipleData: allUsers })
-*/
+    this.setState({ display: false, multipleData: allUsers })
+
   }
 
 
-
-
-
-
-
-  constructor(props) {
-    super(props);
-    this.state = { text: '', display: false, length: '', username: '', singlePlayerData: {}, multipleData: [], menu: '' };
-    this.SearchForUser = this.SearchForUser.bind(this);
-    this.getUserFromID = this.getUserFromID.bind(this);
+  updateIndex(selectedIndex) {
+    console.log("updated: ")
+    console.log(selectedIndex);
+    this.setState({ multipleData: [] });
+    this.setState({ selectedIndex });
+    this.setState({ showStats: true });
+    this.getTopStats(selectedIndex);
   }
 
   render() {
-    this.getRankedUsers();
+    const buttons = ["Mvps", "Goals", "Wins", "Shots", "Saves", "Assists"];
 
-    if (this.state.display === false) {
+    if (this.state.display === false && this.state.showStats === false) {
+      return (
+        <View style={{}}>
+          <Header
+            placement="left"
+            leftComponent={{ icon: 'menu', onPress: () => this.setState({ menu: open }), color: '#fff' }}
+            centerComponent={{ text: 'Rocket League Stats App!', style: { color: '#fff' } }}
+            rightComponent={{ icon: 'home', onPress: () => this.setState({ display: false }), color: '#fff' }}
+            style={{ alignSelf: 'stretch' }}
+          />
+
+          <Text>Welcome to the Rocket league Stats app </Text>
+          <Text>Enter username or steam id</Text>
+          <TextInput style={{ height: 40, width: 200 }} placeholder="Enter username or id here!" onChangeText={(text) => this.setState({ text })} />
+          <Button title="Get Stats" color="#ADD8E6" onPress={() => this.SearchForUser(this.state.text)} />
+
+          <Text>{"\n"}</Text>
+          <CheckBox
+            checked={this.state.checked}
+            onPress={() => this.updateIndex(this.state.selectedIndex)}
+            title="Show stat leaderboard!"
+          />
+        </View>
+      );
+    }
+
+
+    if (this.state.display === false && this.state.showStats === true) {
       return (
 
         <View style={{}}>
@@ -271,6 +326,53 @@ export default class GetUsername extends Component {
           <Text>Enter username or steam id</Text>
           <TextInput style={{ height: 40, width: 200 }} placeholder="Enter username or id here!" onChangeText={(text) => this.setState({ text })} />
           <Button title="Get Stats" color="#ADD8E6" onPress={() => this.SearchForUser(this.state.text)} />
+
+          <Text>{"\n"}</Text>
+          <CheckBox
+            checked={this.state.checked}
+            onPress={() => this.updateIndex(this.state.selectedIndex)}
+            title="Show stat leaderboard!"
+          />
+
+          <ButtonGroup
+            onPress={this.updateIndex}
+            selectedIndex={this.state.selectedIndex}
+            buttons={buttons}
+            containerStyle={{ height: 40 }}
+          />
+
+
+          <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0, }}>
+            <Text>Stat leaderboard for {this.state.stat}:</Text>
+            <FlatList
+              data={this.state.multipleData}
+              renderItem={({ item }) => (
+                <ListItem
+                  roundAvatar
+                  title={`${item.username}`}
+                  subtitle={`Platform: ${item.platform}, ${this.state.stat}: ${item[this.state.stat]}`}
+                  avatar={{ uri: item.avatar }}
+                  containerStyle={{ borderBottomWidth: 0 }}
+                  onPress={() => this.setState({
+                    display: true, singlePlayerData:
+                      {
+                        "Avatar": item.avatar,
+                        "Platform": item.platform,
+                        "Wins": item.wins,
+                        "Goals": item.goals,
+                        "Mvps": item.mvps,
+                        "Saves": item.saves,
+                        "Shots": item.shots,
+                        "Assists": item.assists,
+                        "ProfileUrl": item.profileUrl,
+                        "SignatureUrl": item.signatureUrl
+                      }
+                  })}
+                />
+              )}
+              keyExtractor={item => item.id}
+            />
+          </ List>
         </View>
       );
     }
